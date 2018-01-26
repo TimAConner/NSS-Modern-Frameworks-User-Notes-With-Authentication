0d68d2049c47ea7b311c2e894918384f2c4facca
Tim A. Conner <timaconner1@gmail.com>
2018-01-26 11:40:09 -0600

    Add Firebase Factory.

---
+++ js/factories/AuthFactory.js
@@ -0,0 +1,35 @@
+'use strict';
+const firebase = require("firebase");
+
+module.exports = function($q, $http, $rootScope, FIREBASE_CONFIG){
+    let currentUserData = null;
+
+    // Returns true if user is authenticated.
+    const isAuthenticated = () => {
+        // If currentUser returns data, return true.  Else, return falase.
+        return firebase.auth().currenUser ? true : false; 
+    };
+
+    // Returns email, user id of currently logged in user.
+    const getCurUser = () => {
+        return firebase.auth().currentUser();
+    };
+
+    // Logs current user out.
+    const logOut = () => {
+        firebase.auth().signOut();
+    };
+
+    // Takes one argument, an object with email and password property.  It will login the user on firebase with the email and password they have given.
+    const logIn = (user) => {
+        return firebase.auth().signInWithEmailAndPassword(user.email, user.password);
+    };
+
+    // Takes an object with an email and password.  Registers a new user with firebase.
+    const registerWithEmail = (user) => {
+        return firebase.auth().createUserWithEmailAndPassword(user.email, user.password);
+    };
+
+    return {isAuthenticated, getCurUser, logOut, logIn, registerWithEmail};
+
+};
