3ff2f7145003861141fa4d97d0def0d831caf6a4
Tim A. Conner <timaconner1@gmail.com>
2018-01-26 15:34:45 -0600

    Remove extra console log.

--- js/controllers/NoteListCtrl.js
+++ js/controllers/NoteListCtrl.js
@@ -4,7 +4,6 @@ module.exports = function($scope, $window, NoteFactory, AuthFactory){
     let user = AuthFactory.getCurUser();
     if(user){
         NoteFactory.getNotes(user.uid).then(noteData => {
-            console.log('Object.values(noteData.data)', Object.values(noteData.data));
             $scope.noteList = Object.values(noteData.data);
         });
     } else {
