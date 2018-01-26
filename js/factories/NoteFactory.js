b4ff7bbb2fc9d89a881749567b2f2615357e4044
Tim A. Conner <timaconner1@gmail.com>
2018-01-26 14:13:31 -0600

    Add New Note Factory.

---
+++ js/factories/NoteFactory.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = function($q, $http){
+
+    const getNotes = (userId) => {
+
+    };
+
+    const saveNote = (note, uid) => {
+        return $q((resolve, reject) => {
+            $http
+            .post("https://practice-project-d42d4.firebaseio.com/UserNotesWithAuth.json", 
+                {
+                    note,
+                    uid
+                }
+            );
+        });
+    };
+};
